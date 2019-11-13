68757a48cc451d31dfa92b03ef3c04672f382b58
k-nasa <htilcs1115@gmail.com>
2019-11-13 23:49:41 +0900

    fix sha

--- goku.rb
+++ goku.rb
@@ -3,7 +3,7 @@ class Goku < Formula
   homepage "https://github.com/k-nasa/goku"
   url "https://github.com/k-nasa/goku/releases/download/0.1.1/goku_x86_64-apple-darwin.tar.gz"
   version "0.1.1"
-  sha256 "ab3f39d17ad57c37b4ac78d6bc67b91541bcbd887fcd3f42c2380b00537168e6"
+  sha256 "15e74f4d7f3dde16fdad76349d7725b077b1e9bbbfb6aa829efddf8c30a29273"
 
   def install
     bin.install "goku"
