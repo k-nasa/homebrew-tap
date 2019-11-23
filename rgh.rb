675105ae81eba14d0cea0f668c2b9a6cb627b61c
k-nasa <htilcs1115@gmail.com>
2019-11-23 22:33:48 +0900

    fix sha

--- rgh.rb
+++ rgh.rb
@@ -3,7 +3,7 @@ class Rgh < Formula
   homepage "https://github.com/k-nasa/rgh"
   url "https://github.com/k-nasa/rgh/releases/download/0.2.0/rgh_x86_64-apple-darwin.tar.gz"
   version "0.2.0"
-  sha256 "ab3f39d17ad57c37b4ac78d6bc67b91541bcbd887fcd3f42c2380b00537168e6"
+  sha256 "8ecc01c8be96c67c6091e9a5df2f442d01dedeb056c1e7a27f93a6b3288cfcc7"
 
   def install
     bin.install "rgh"
