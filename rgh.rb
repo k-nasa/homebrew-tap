087816f0d994e500804d5c8140303f7d3912b6d8
k-nasa <htilcs1115@gmail.com>
2019-11-23 22:31:07 +0900

    update rgh version

--- rgh.rb
+++ rgh.rb
@@ -1,8 +1,8 @@
 class Rgh < Formula
   desc "Creates GitHub release and upload asset files"
   homepage "https://github.com/k-nasa/rgh"
-  url "https://github.com/k-nasa/rgh/releases/download/0.1.0/rgh_x86_64-apple-darwin.tar.gz"
-  version "0.1.0"
+  url "https://github.com/k-nasa/rgh/releases/download/0.2.0/rgh_x86_64-apple-darwin.tar.gz"
+  version "0.2.0"
   sha256 "ab3f39d17ad57c37b4ac78d6bc67b91541bcbd887fcd3f42c2380b00537168e6"
 
   def install
