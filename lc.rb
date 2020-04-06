d12f2f98a1e1b76e7b43e8923f12f920d55dea3a
k-nasa <htilcs1115@gmail.com>
2020-04-06 22:21:15 +0900

    fix

--- lc.rb
+++ lc.rb
@@ -1,7 +1,7 @@
 class Lc < Formula
   desc "Link checker"
   homepage "https://github.com/k-nasa/lc"
-  url "https://github.com/k-nasa/goku/releases/download/0.1.0/lc_x86_64-apple-darwin.tar.gz"
+  url "https://github.com/k-nasa/lc/releases/download/0.1.0/lc_x86_64-apple-darwin.tar.gz"
   version "0.1.0"
 
   def install
