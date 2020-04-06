ac83e88b012a633b71acaa1c1e4fe407a959f17a
k-nasa <htilcs1115@gmail.com>
2020-04-06 22:24:41 +0900

    add sha256

--- lc.rb
+++ lc.rb
@@ -3,6 +3,7 @@ class Lc < Formula
   homepage "https://github.com/k-nasa/lc"
   url "https://github.com/k-nasa/lc/releases/download/0.1.0/lc_x86_64-apple-darwin.tar.gz"
   version "0.1.0"
+  sha256 "af2c58c594d2c8a99e90636754e17e0f75b92974dd2770fc3484869597ab98dd"
 
   def install
     bin.install "lc"
