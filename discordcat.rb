b6a8a08d44976ae395db62d08e9ef6e182ef9882
k-nasa <htilcs1115@gmail.com>
2021-06-19 18:14:45 +0900

    update sha

--- discordcat.rb
+++ discordcat.rb
@@ -5,7 +5,7 @@ class Discordcat < Formula
   homepage 'https://github.com/k-nasa/discordcat'
   url "https://github.com/k-nasa/discordcat/releases/download/#{version}/discordcat_x86_64-apple-darwin.tar.gz"
   version version
-  sha256 '15e74f4d7f3dde16fdad76349d7725b077b1e9bbbfb6aa829efddf8c30a29273'
+  sha256 '8f8474348b62ca9ee2599456013deb746a8e546684c1f151e627f4947894ba33'
 
   def install
     bin.install 'discordcat'
