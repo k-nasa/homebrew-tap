83743ce001686b7b10f39a785b292afb142d0576
k-nasa <htilcs1115@gmail.com>
2021-06-27 12:53:30 +0900

    update

--- wai.rb
+++ wai.rb
@@ -4,6 +4,7 @@ class Wai < Formula
   desc 'wai - A simple wasm interpreter'
   homepage 'https://github.com/k-nasa/wai'
   url "https://github.com/k-nasa/wai/releases/download/#{version}/wai_x86_64-apple-darwin.tar.gz"
+  sha256 '40eafa807dd83ca9a7343757d2c16abaf2f4b4993776e30c2c4ab096f7b2bf71'
   version version
 
   def install
